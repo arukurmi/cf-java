c7a585b95f8fe7dce42ff1f7eb37f8a9a529446d
Aryansh Kurmi <arukurmi22@gmail.com>
2026-06-12 21:02:15 +0530

    add boilerplate cp code

---
+++ boiler.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main{
+
+    private static int solve(){
+        int n;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+    }
+
+
+    public static void main(String[] args){
+        int t;
+        Scanner sc = new Scanner(System.in);
+        t = sc.nextInt();
+        while(t-- > 0){
+            System.out.println(solve());
+        }
+    }
+}
